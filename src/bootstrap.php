3369889bb2d1216e71c986d9691004df2c6004ab
Michele Locati <michele@locati.it>
2019-09-30 15:48:39 +0200

    Allow using the package as a Composer dependency

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,15 +5,22 @@ declare(strict_types=1);
 use Dotenv\Dotenv;
 use MLocati\C5SinceTagger\Application;
 
-$autoload = __DIR__ . '/../vendor/autoload.php';
+$autoload = dirname(__DIR__) . '/vendor/autoload.php';
 if (is_file($autoload)) {
     require_once $autoload;
+    $rootDir = dirname(__DIR__);
+} else {
+    $autoload = dirname(__DIR__, 3) . '/autoload.php';
+    if (is_file($autoload)) {
+        require_once $autoload;
+        $rootDir = dirname(__DIR__, 4);
+    } else {
+        fprintf(STDERR, 'You must install the dependencies using `composer install`' . PHP_EOL);
+        exit(1);
+    }
 }
 unset($autoload);
-if (!class_exists(Application::class)) {
-    fprintf(STDERR, 'You must install the dependencies using `composer install`' . PHP_EOL);
-    exit(1);
-}
-Dotenv::create([__DIR__ . '/..'])->load();
+Dotenv::create($rootDir)->load();
+unset($rootDir);
 
 return new Application();
